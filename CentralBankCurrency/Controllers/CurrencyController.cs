408be678be15850bf28647b4a65a7bef87f5212b
Çağrı <cagritumer0@gmail.com>
2022-05-11 15:53:35 +0300

    Add project files.

---
+++ CentralBankCurrency/Controllers/CurrencyController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Xml;
+
+namespace CentralBankCurrency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetCurrency(decimal amount, string from, string to)
+        {
+            XmlDocument doviz = new XmlDocument();
+            doviz.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+
+            var kodFrom = doviz.SelectSingleNode($"//Currency[@Kod=\"{from}\"]/ForexBuying");
+            var kodTo = doviz.SelectSingleNode($"//Currency[@Kod=\"{to}\"]/ForexBuying");
+            if (kodFrom == null || kodTo == null)
+            {
+                return this.StatusCode(406, "Girilen Para Birimi Hatalı");
+
+            }
+            else
+            {
+                decimal valueFrom = Convert.ToDecimal(kodFrom.InnerText);
+                decimal valueTo = Convert.ToDecimal(kodTo.InnerText);
+                decimal sonuc = Math.Round(((valueFrom / valueTo) * amount),2);
+                string sonucYeni = String.Format("{0:#,0.000}", sonuc);
+                string amounYeni = String.Format("{0:#,0.000}", amount);
+                string text = $"{amounYeni} {from} Equals To {sonucYeni}{to}";
+
+                return this.StatusCode(200, text);
+
+            }
+
+
+        }
+    }
+}
---
+++ CentralBankCurrency/Controllers/CurrencyListController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace CentralBankCurrency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyListController : ControllerBase
+    {
+        [HttpGet]
+        public List<dynamic> GetCurrencies()
+        {
+            List<dynamic> currencyName = new List<dynamic>();
+
+            XmlDocument doviz = new XmlDocument();
+            doviz.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+            var currencies = doviz.SelectNodes($"//Currency");
+
+            foreach (XmlNode currenciesNode in currencies)
+            {
+                string kod = currenciesNode.Attributes["Kod"].Value;
+                string isim = currenciesNode.SelectSingleNode("Isim").InnerText;
+                currencyName.Add(new { Name = isim, Code = kod });
+
+            }
+            return currencyName;
+        }
+    }
+}
